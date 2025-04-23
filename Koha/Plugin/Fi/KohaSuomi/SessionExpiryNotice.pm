4f03f1957861bdb9c23674ba8f3e561afb3c1f3f
lmstrand <lmstrand@gmail.com>
2025-04-23 15:19:13 +0300

    Add script and config

--- Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice.pm
+++ Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice.pm
@@ -22,7 +22,7 @@ our $metadata = {
     minimum_version => '23.11',
     maximum_version => '',
     version         => $VERSION,
-    description     => "",
+    description     => "Luo ilmoituksen popup-ikkunaan virkailijaliittymään kirjautuneen käyttäjän istunnon vanhenemisesta. Skripti ilmoittaa vanhenemisesta viisi minuuttia etukäteen ja uudelleen istunnon vanhennettua. Ilmoitusaikaa voi muuttaa pluginin asetuksella, johon tulee täyttää kimpan timeout-järjestelmäasetusta vastaava aika.",
 };
 
 ## This is the minimum code required for a plugin's 'new' method
@@ -53,10 +53,10 @@ sub intranet_js {
     my $plugin_fulldir = $dir . "/Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice/";
     my $js = read_file($plugin_fulldir .'script.js');
     
-    # my $param_a = $self->retrieve_data('config_param_a');
+    my $param_a = $self->retrieve_data('config_param_a') || 1800;
     
-    ## Add REPLACE_BY_CONFIG_PARAM_A to the js script to replace it with the configuration parameter
-    # $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_A/$param_a/r;
+    # Add REPLACE_BY_CONFIG_PARAM_A to the js script to replace it with the configuration parameter
+    $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_A/$param_a/r;
     
     utf8::decode($js);
     return "<script>$js</script>";
@@ -75,10 +75,10 @@ sub tool {
     my $plugin_fulldir = $self->mbf_path();
     my $js = read_file($plugin_fulldir .'script.js');
     
-    # my $param_a = $self->retrieve_data('config_param_a');
+    my $param_a = $self->retrieve_data('config_param_a') || 1800;
     
-    ## Add REPLACE_BY_CONFIG_PARAM_A to the js script to replace it with the configuration parameter
-    # $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_A/$param_a/r;
+    # Add REPLACE_BY_CONFIG_PARAM_A to the js script to replace it with the configuration parameter
+    $js = $js =~ s/REPLACE_BY_CONFIG_PARAM_A/$param_a/r;
     
     utf8::decode($js);
     $template->param( 'jscontent' => $js );
@@ -86,35 +86,35 @@ sub tool {
     $self->output_html( $template->output() );
 }
 
-## If your tool is complicated enough to needs it's own setting/configuration
-## you will want to add a 'configure' method to your plugin like so.
-## Here I am throwing all the logic into the 'configure' method, but it could
-## be split up like the 'report' method is.
-# sub configure {
-#     my ( $self, $args ) = @_;
-#     my $cgi = $self->{'cgi'};
+# If your tool is complicated enough to needs it's own setting/configuration
+# you will want to add a 'configure' method to your plugin like so.
+# Here I am throwing all the logic into the 'configure' method, but it could
+# be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
 
-#     unless ( $cgi->param('save') ) {
-#         my $template = $self->get_template({ file => 'configure.tt' });
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
 
-#         ## Grab the values we already have for our settings, if any exist
-#         $template->param(
-#             config_param_a => $self->retrieve_data('config_param_a'),
-#             last_upgraded   => $self->retrieve_data('last_upgraded'),
-#         );
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            config_param_a => $self->retrieve_data('config_param_a') || 1800,
+            last_upgraded   => $self->retrieve_data('last_upgraded'),
+        );
 
-#         $self->output_html( $template->output() );
-#     }
-#     else {
-#         $self->store_data(
-#             {
-#                 config_param_a => $cgi->param('config_param_a'),
-#                 last_configured_by => C4::Context->userenv->{'number'},
-#             }
-#         );
-#         $self->go_home();
-#     }
-# }
+        $self->output_html( $template->output() );
+    }
+    else {
+        $self->store_data(
+            {
+                config_param_a => $cgi->param('config_param_a'),
+                last_configured_by => C4::Context->userenv->{'number'},
+            }
+        );
+        $self->go_home();
+    }
+}
 
 ## This is the 'install' method. Any database tables or other setup that should
 ## be done when the plugin if first installed should be executed in this method.
--- Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice/script.js
+++ Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice/script.js
@@ -1,1 +1,37 @@
-
+// Ilmoitus istunnon vanhenemisesta
+$(document).ready(function() {
+    if ($('body#main_auth').length == 0) {
+      const timeoutLength = REPLACE_BY_CONFIG_PARAM_A;
+      let timeoutTime = new Date();
+      timeoutTime.setSeconds(timeoutTime.getSeconds() + timeoutLength);
+      localStorage.setItem('KohaTimeoutAlertStatus', 0);
+      localStorage.setItem('KohaTimeoutTime', timeoutTime);
+      // Function to check if the last accessed time is still valid
+      function checkLastAccessedTime() {
+        let currentTime = new Date();
+        let timeoutAlertStatus = localStorage.getItem('KohaTimeoutAlertStatus');
+        let timeoutTime = new Date(localStorage.getItem('KohaTimeoutTime'));
+        let timeDifference = timeoutTime - currentTime;
+        let minutesDifference = Math.floor(timeDifference / 60000); // Convert milliseconds to minutes  
+        
+        // Check if there are 5 minutes left
+        if (minutesDifference <= 5 && timeoutAlertStatus == 0) {
+            localStorage.setItem('KohaTimeoutAlertStatus', 1);
+            if(!alert('Istunto vanhenee klo ' + timeoutTime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }) +'. Paina OK ja tee Kohassa jokin toiminto esim. etusivun lataaminen jatkaaksesi istuntoa.')) {
+  
+            }
+        }
+        // Check if timeoutTime has gone and display new message
+        if (currentTime > timeoutTime && timeoutAlertStatus != 2){
+            localStorage.setItem('KohaTimeoutAlertStatus', 2);
+            if(!alert('Istunto vanhentunut klo ' + timeoutTime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }) +'. Paina OK ja kirjaudu uudelleen Kohaan.')) {
+            
+            }
+        }
+      }
+    
+  
+      // Call the checkLastAccessedTime function every minute
+      setInterval(checkLastAccessedTime, 60000);
+    }
+  });
