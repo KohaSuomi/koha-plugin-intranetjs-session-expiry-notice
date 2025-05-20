6e0f60dcb04e931f65225838882f244e4c9d84d9
lmstrand <lmstrand@gmail.com>
2025-05-20 11:28:44 +0300

    KOHA-1501: Add info where plugin is used

--- Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice.pm
+++ Koha/Plugin/Fi/KohaSuomi/SessionExpiryNotice.pm
@@ -15,14 +15,14 @@ our $VERSION = "1.0.0";
 
 my $description = "";
 
-my $lang = C4::Languages::getlanguage();
+my $lang = C4::Languages::getlanguage() ||'fi-FI';
 
 if ($lang eq 'sv-SE') {
-    $description = "Skapar ett meddelande i ett popup-fönster i personalgränssnittet för att informera användaren om att sessionen håller på att gå ut. Skriptet varnar användaren fem minuter innan sessionen går ut och igen när sessionen har gått ut. Tiden för varningen kan ändras i plugin-inställningarna, där den bör matcha systeminställningen för timeout.";
+    $description = "Skapar ett meddelande i ett popup-fönster i personalgränssnittet för att informera användaren om att sessionen håller på att gå ut. Skriptet varnar användaren fem minuter innan sessionen går ut och igen när sessionen har gått ut. Tiden för varningen kan ändras i plugin-inställningarna, där den bör matcha systeminställningen för timeout. (Local databases, Täti)";
 } elsif ($lang eq 'en') {
-    $description = "Creates a notification in a popup window in the staff interface to inform the logged-in user that their session is about to expire. The script warns the user five minutes before the session expires and again after it has expired. The warning time can be adjusted in the plugin settings to match the system timeout configuration.";
+    $description = "Creates a notification in a popup window in the staff interface to inform the logged-in user that their session is about to expire. The script warns the user five minutes before the session expires and again after it has expired. The warning time can be adjusted in the plugin settings to match the system timeout configuration. (Lokala databaser, Täti)";
 } else {
-    $description = "Luo ilmoituksen popup-ikkunaan virkailijaliittymään kirjautuneen käyttäjän istunnon vanhenemisesta. Skripti ilmoittaa vanhenemisesta viisi minuuttia etukäteen ja uudelleen istunnon vanhennettua. Ilmoitusaikaa voi muuttaa pluginin asetuksella, johon tulee täyttää kimpan timeout-järjestelmäasetusta vastaava aika.";
+    $description = "Luo ilmoituksen popup-ikkunaan virkailijaliittymään kirjautuneen käyttäjän istunnon vanhenemisesta. Skripti ilmoittaa vanhenemisesta viisi minuuttia etukäteen ja uudelleen istunnon vanhennettua. Ilmoitusaikaa voi muuttaa pluginin asetuksella, johon tulee täyttää kimpan timeout-järjestelmäasetusta vastaava aika. (Paikalliskannat, Täti)";
 }
 
 ## Here is our metadata, some keys are required, some are optional
